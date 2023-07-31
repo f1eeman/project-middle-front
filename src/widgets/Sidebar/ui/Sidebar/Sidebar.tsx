d5bd9b23325bad87b1fec9cad737ab86523e5d7a
f1eeman <mr.f1eeman@gmail.com>
2023-07-31 17:15:58 +0600

    add screenshot testing to ci

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -26,7 +26,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 data-testid="sidebar-toggle"
                 onClick={onToggle}
             >
-                {t('toggle')}
+                {t('Переключить')}
             </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
